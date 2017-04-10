76d6ea63f6a0f5be9da2f995540cf6ae8e2a5c99
Dom <v.domanchuk@favbet.com>
2017-04-10 10:16:24 +0300

    add assignment 2 10

---
+++ week2/frequency_10.erl
@@ -0,0 +1,128 @@
+%% Based on code from 
+%%   Erlang Programming
+%%   Francecso Cesarini and Simon Thompson
+%%   O'Reilly, 2008
+%%   http://oreilly.com/catalog/9780596518189/
+%%   http://www.erlangprogramming.org/
+%%   (c) Francesco Cesarini and Simon Thompson
+
+-module(frequency_10).
+-export([allocate/0,deallocate/1,stop/0]).
+-export([init_worker/0]).
+-export([start_supervisor/0, init_supervisor/0, stop_supervisor/0]).
+
+%% These are the start functions used to create and
+%% initialize the server.
+
+%start() ->
+%    register(frequency,
+%	     spawn(frequency, init, [])).
+
+init_worker() ->
+  register(frequency, self()),
+  process_flag(trap_exit, true),    %%% ADDED
+  Frequencies = {get_frequencies(), []},
+  loop(Frequencies).
+
+% Hard Coded
+get_frequencies() -> [10,11,12,13,14,15].
+
+%% The Main Loop
+
+loop(Frequencies) ->
+  receive
+    {request, Pid, allocate} ->
+      {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+      Pid ! {reply, Reply},
+      loop(NewFrequencies);
+    {request, Pid , {deallocate, Freq}} ->
+      NewFrequencies = deallocate(Frequencies, Freq),
+      Pid ! {reply, ok},
+      loop(NewFrequencies);
+    {request, Pid, stop} ->
+      Pid ! {reply, stopped};
+    {'EXIT', Pid, _Reason} ->                   %%% CLAUSE ADDED
+      NewFrequencies = exited(Frequencies, Pid), 
+      loop(NewFrequencies)
+  end.
+
+%% Functional interface
+
+allocate() -> 
+    frequency ! {request, self(), allocate},
+    receive 
+	    {reply, Reply} -> Reply
+    end.
+
+deallocate(Freq) -> 
+    frequency ! {request, self(), {deallocate, Freq}},
+    receive 
+	    {reply, Reply} -> Reply
+    end.
+
+stop() -> 
+    frequency ! {request, self(), stop},
+    receive 
+	    {reply, Reply} -> Reply
+    end.
+
+
+%% The Internal Help Functions used to allocate and
+%% deallocate frequencies.
+
+allocate({[], Allocated}, _Pid) ->
+  {{[], Allocated}, {error, no_frequency}};
+allocate({[Freq|Free], Allocated}, Pid) ->
+  link(Pid),                                               %%% ADDED
+  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+
+deallocate({Free, Allocated}, Freq) ->
+  {value,{Freq,Pid}} = lists:keysearch(Freq,1,Allocated),  %%% ADDED
+  unlink(Pid),                                             %%% ADDED
+  NewAllocated=lists:keydelete(Freq, 1, Allocated),
+  {[Freq|Free],  NewAllocated}.
+
+exited({Free, Allocated}, Pid) ->                %%% FUNCTION ADDED
+    case lists:keysearch(Pid,2,Allocated) of
+      {value,{Freq,Pid}} ->
+        NewAllocated = lists:keydelete(Freq,1,Allocated),
+        {[Freq|Free],NewAllocated}; 
+      false ->
+        {Free,Allocated} 
+    end.
+
+
+
+start_supervisor() ->
+    register(supervisor, spawn(?MODULE, init_supervisor, [])).
+
+init_supervisor() ->
+    process_flag(trap_exit, true),
+    Pid = start_worker(),
+    loop_supervisor(Pid).
+
+start_worker() ->
+    spawn_link(?MODULE, init_worker, []).
+
+loop_supervisor(Pid) ->
+    receive
+        {'EXIT', Pid, Why} ->
+            io:format("Server stopped because ~p~n", [Why]),
+            NewPid = start_worker(),
+            loop_supervisor(NewPid);
+        {stop, From} ->
+            io:format("Supervisor stopped!~n"),
+            exit(Pid, kill),
+            From ! {reply, ok}
+    end.
+
+stop_supervisor() ->
+    supervisor ! {stop, self()},
+    receive
+        {reply, Reply} ->
+            Reply
+    end.
+
+
+
+
